2ca3340e0bc7f68ca9f8e63ab0fcd22ba0a9561d
dorokdok <90540407+dorokdok@users.noreply.github.com>
2022-03-22 17:50:12 +0700

    Last

--- fungsi.py
+++ fungsi.py
@@ -1,5 +1,4 @@
-from audioop import reverse
-from random import choices, randrange, random
+from random import choices, randint, randrange, random
 from typing import List
 import math
 
@@ -8,36 +7,107 @@ Populasi = List[Genome]
 Rank = List[float]
 Cross = List[Genome]
 Parent = List[Genome]
+Dekode = List[float]
 
+#Pembuatan kromosom menggunakan binary encoding
 def buat_genome(length: int) -> Genome:
     return choices([0,1], k=length)
 
+#Pembuatan populasi berdasarankan ukuran populasi dan panjang kromosom/gen
 def buatPopulasi(size: int, panjangGen : int) -> Populasi:
     return [buat_genome(panjangGen) for i in range(size)]
 
-#Masukkin rumus fitness di sini decode genome ke float dulu baru masukkin sama rumus tugas
-def fitness(gen: Genome) -> float:
-    return
-def rankFit(populasi: Populasi) -> Rank:
-    return [fitness(populasi[i]) for i in range(len(populasi))].sort()
+#Dekode untuk kromosom menggunakan decoder binary encoder
+def decodeKro(gen:Genome,min: int, max: int)-> Dekode:
+    xB = 0
+    xA = 0
+    p = len(gen)//2
+    for i in range(p):
+        xB = xB + (2**-(i+1))
+    for i in range(p):
+        xA = xA + gen[i]*(2**-(i+1))
+    x = min + ((max-min)/xB)*xA
 
-def sortPopulasi(populasi: Populasi, rankFit : Rank) -> Populasi:
-    return [x for _, x in sorted(zip(rankFit,Populasi))]
+    yB = 0
+    yA = 0
+    p2 = p+1
+    i = 1
+    while p2 < len(gen):
+        yB = yB + (2**-i)
+        p2 += 1
+        i += 1
+    i = 1
+    p2 = p+1
+    while p2 < len(gen):
+        yA = yA + gen[p2]*2**(-i)
+        i += 1
+        p2 += 1
+    y = min + ((max-min)/yB)*yA
+    return [x,y]
 
+
+def fitness(gen: Dekode) -> float:
+    return ((math.cos(gen[0])+math.sin(gen[1]))**2/(gen[0]**2)*(gen[1]**2))
+
+#Pemilihan orang tua menggunakan Roulette wheel Selection di mana kromosom yang terbaik akan dipilih
 def selectParent(populasi: Populasi, nilai : Rank) -> Parent:
     return choices(populasi, weights = nilai.reverse(), k=2)
 
+#Probabilitas operasi crossover menggunakan nilai 0.6 / 60%
 def crossover(a: Genome, b: Genome)-> Cross:
-    p = int(len(a)/2)
-    return [a[0:p] + b[p:len(a)], b[0:p] + a[p:len(a)]]
+    prob = choices([0,1], weights = [0.4,0.6], k = 1)
+    if prob[0] == 1:
+        p = randint(1, len(a))
+        return [a[0:p] + b[p:len(a)], b[0:p] + a[p:len(b)]]
+    else:
+        return [a,b]
 
-def mutation(gen: Genome, num : int = 1, prob : float = 0.5)-> Genome:
+#Probabilitas operasi mutasi menggunakan 0.5(terdapat pada fungsi evolusi di bawah)
+def mutation(gen: Genome, num : int, prob : float)-> Genome:
     for i in range(num):
         idx = randrange(len(gen))
-        if random() > prob :
-            gen[idx] = gen[idx] 
-        else:
-            abs(gen[idx]-1)
+        gen[idx] = gen[idx] if random() > prob else abs(gen[idx]-1)
     return gen
 
+#Fungsi untuk melakukan evolusi dengan inputan populasi yang diinginkan, panjang kromosom yang dibutuhkan, 
+# dan rentang minimal dan maksimal.
+def evolution(popu: int, kromo: int, min: int, max: int) -> Genome:
+    p = Populasi
+    p = buatPopulasi(popu, kromo)
+    gen = 0
+    while True:
+        newP = Populasi
+        newP = []
+        ranking = Rank
+        ranking = []
+        for j in range(len(p)):
+            c = Dekode
+            c = decodeKro(p[j], min, max)
+            ranking.append(fitness(c))
+        p = [x for _, x in sorted(zip(ranking,p))]
+        ranking.sort()
+        #Tempat melakukan seleksi survivor dengan metode Elitism yang akan memilih 1 kromosom jika populasi ganjil dan 2 jika genap
+        if popu % 2 == 0:
+            newP.append(p[0])
+            newP.append(p[1])
+        else :
+            newP.append(p[0])
+        while len(newP) != len(p):
+            par = selectParent(p, ranking)
+            silang = Cross
+            silang = crossover(par[0],par[1])
+            mutA = Genome
+            mutB = Genome
+        #Mutasi penggunakan probabilitas 0.5%
+            mutA = mutation(silang[0],1,0.5)
+            mutB = mutation(silang[1],1,0.5)
+            newP.append(mutA)
+            newP.append(mutB)
+        p = newP
+        gen += 1
+        #Perhentian generasi dilakukan jadi terdapat 3 kromosom dengan nilai sama
+        if p[0] == p[len(p)//2]:
+            break
 
+    print("Generasi: ", gen)
+    return p[0]
--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import fungsi as f
 
+genakhir = f.Genome
+genDekode = f.Dekode
 
-a = f.Populasi
-a = f.buatPopulasi(9, 6)
-print(a)
-b = f.Rank
-b = [1,5,8,9,10,11,13,15]
+populasi = int(input("Masukkan banyak populasi dalam satu generasi: "))
+kromosom = int(input("Masukkan panjang kromosom/gen yang ingin digunakan: "))
 
-c = f.Parent
-
-c = f.selectParent(a,b)
-print(c)
-c = f.selectParent(a,b)
-print(c)
-c = f.selectParent(a,b)
-print(c)
-c = f.selectParent(a,b)
-print(c)
+genakhir = f.evolution(populasi,kromosom,-5,5)
+genDekode = f.decodeKro(genakhir, -5,5)
+print("Kromsom Terbaik", genakhir)
+print("X= ", genDekode[0], "Y= ", genDekode[1])
+print("Nilai akhir= ",f.fitness(genDekode))
